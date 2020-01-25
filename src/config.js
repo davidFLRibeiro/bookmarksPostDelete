4a1e4291e7788aabc00a50c502b7c3f857c96b90
David Ribeiro <davidflribeiro87@gmail.com>
2020-01-25 17:12:21 -0100

    first commit

---
+++ src/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  PORT: process.env.PORT || 1000,
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  API_TOKEN: process.env.API_TOKEN || '910237e9-95fd-4ecf-b17b-4af6605a1f01',
+  DB_URL:
+    process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/bookmarks'
+};
